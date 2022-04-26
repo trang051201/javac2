517e50fe932280d8bd8329402bf9cf4d9ceab467
trang051201 <trangtrau201@gmail.com>
2022-04-26 23:05:24 +0700

    upload

---
+++ src/HovaTenSV_App/Lehuuvan.java
@@ -0,0 +1,10 @@
+package HovaTenSV_App;
+
+public class Lehuuvan {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    System.out.println("Hello Văn Học Java");
+	}
+
+}
